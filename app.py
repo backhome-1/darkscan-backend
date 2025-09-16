b97dfe6bcde83d1b2a38f60afcb009df82c2d42b
backhome-1 <william.lee.bbc@gmail.com>
2025-09-16 04:48:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # 모든 도메인 허용
 
 import os
 import json
